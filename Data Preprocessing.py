db72bbf06a9f56c3d9d4f3e6914cffb7ce43f9e5
Braulio Otavalo <56369660+beotavalo@users.noreply.github.com>
2021-04-07 10:15:16 -0500

    Add files via upload

---
+++ Data Preprocessing.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  6 15:33:57 2021
+
+@author: braul
+"""
+
+import pandas as pd
+df = pd.read_csv('C:/Users/braul/OneDrive/Documentos/Braulio Otavalo/Projects/KISTH/KIPUS RG/Defunciones/2. Datos_abiertos_EDG_2019/BDD_EDG_2019.csv', encoding='utf-8',sep=";")
+df.info()
